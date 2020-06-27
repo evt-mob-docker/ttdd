efc9b25b9d755b7d918da1bfa2ecbd40268bac20
nobishino <nobishii11@gmail.com>
2020-06-27 13:44:33 +0900

    variable money

--- money.test.ts
+++ money.test.ts
@@ -1,7 +1,9 @@
 import Dollar from './money'
 
 test('Dollar', () => {
-    let five = new Dollar(5);
-    five.times(2);
-    expect(five.amount).toBe(10);
+    let mymoney = new Dollar(5);
+    mymoney.times(2);
+    expect(mymoney.amount).toBe(10);
+    mymoney.times(3);
+    expect(mymoney.amount).toBe(15);
 })
--- money.ts
+++ money.ts
@@ -1,12 +1,14 @@
 class Dollar {
     public amount: number;
+    private initialAmount: number;
 
     constructor(amount: number) {
+        this.initialAmount = amount;
         this.amount = amount;
     }
 
     times(multiplier: number) {
-        this.amount *= multiplier;
+        this.amount = multiplier * this.initialAmount;
     }
 
 }
